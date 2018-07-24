5a5c7d8309f5bdb9758cef5d81853c10794891d3
dj458 <fzheng8@vt.edu>
2018-07-24 14:28:06 -0400

    use label tag for finding duplicated array elements

---
+++ src/main/java/com/Bike/core/LabelDuplicatedArray.java
@@ -0,0 +1,16 @@
+package com.Bike.core;
+
+public class LabelDuplicatedArray {
+    public static void main (String[] arg){
+        Integer [] input={4,3,2,7,8,2,3,1};
+        int[] labelArray = new int[9];
+
+        for (int i=0;i<input.length;i++){
+            int arrayValue=input[i];
+            labelArray[arrayValue]++;
+            if(labelArray[arrayValue]>1){
+               System.out.println(arrayValue);
+           }
+        }
+    }
+}
