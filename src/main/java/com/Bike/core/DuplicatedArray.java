a0835dbeccd3d0086b83b5f780bdb5f8d8f424df
dj458 <fzheng8@vt.edu>
2018-07-24 14:12:27 -0400

    bubble sort + find duplicated array element

--- src/main/java/com/Bike/core/DuplicatedArray.java
+++ src/main/java/com/Bike/core/DuplicatedArray.java
@@ -1,14 +1,30 @@
 package com.Bike.core;
 
+
+import java.util.Arrays;
+
 public class DuplicatedArray {
 
     public static void main(String[] args ) throws Exception {
 
         Integer [] input={4,3,2,7,8,2,3,1};
-        for(int i=0;i<input.length;i++){
-            
-
+        for(int i=0;i <=input.length-1;i++){
+            for(int j=i+1;j<=input.length-1;j++){
+            if (input[i]>input[j]) {
+                int temp=input[i];
+                input[i]=input[j];
+                input[j]=temp;
+              }
             }
+            }
+        System.out.println(Arrays.toString(input));
+        for(int k=1;k<input.length-2;k++){
+            if((input[k]==input[k-1])){
+                System.out.println(input[k]);
+            }
+
+        }
         }
     }
+
 
