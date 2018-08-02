4cbec84e80c11700a35da79c4adafa8cc4106361
dj458 <fzheng8@vt.edu>
2018-08-01 23:58:42 -0400

    add hashSet

---
+++ src/main/java/com/Bike/core/HashSetExample.java
@@ -0,0 +1,42 @@
+package com.Bike.core;
+
+import java.util.*;
+
+public class HashSetExample {
+    public static void main(String[] args){
+        HashSet hashSet=new HashSet();
+        hashSet.add("abc");
+
+
+
+        hashSet.clear();
+
+
+
+        hashSet.add("abc");
+
+        hashSet.clone();
+
+        hashSet.forEach((s) -> {
+            System.out.println(s);
+        });
+        hashSet.add("abc");
+        boolean a= hashSet.contains("ab");
+        System.out.println(a);
+        hashSet.add("abc");
+        boolean b=hashSet.isEmpty();
+        System.out.println(b);
+        hashSet.add("abc");
+        hashSet.iterator();
+        hashSet.add("bcd");
+        hashSet.remove("abc");
+        hashSet.forEach((s) -> {
+            System.out.println(s);
+        });
+        hashSet.add("abc");
+        int s=hashSet.size();
+        System.out.println(s);
+        hashSet.add("abc");
+        hashSet.spliterator();
+    }
+}
