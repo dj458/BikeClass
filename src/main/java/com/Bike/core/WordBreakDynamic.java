d80be9be9e4c9a9e17a1294d891cc146c7e6c00d
dj458 <fzheng8@vt.edu>
2018-08-09 18:46:59 -0400

    successfully finished the word break problem in dynamic method

---
+++ src/main/java/com/Bike/core/WordBreakDynamic.java
@@ -0,0 +1,48 @@
+package com.Bike.core;
+
+import java.util.*;
+
+public class WordBreakDynamic {
+    int i=0;
+
+    public void wordBreakSolution1 (String s, List<String> wordDict)
+    {
+        List<Character> createdWord = new ArrayList<>();
+        char[] charArray = s.toCharArray();
+        for (int j = i; j<charArray.length; j++)
+        {
+            createdWord.add(charArray[j]);
+            StringBuilder b = new StringBuilder();
+            createdWord.forEach(b::append);
+            String bs = b.toString();
+
+            for (int k = 0;k<wordDict.size();k++)
+            {
+
+                if(bs.equals(wordDict.get(k)))
+                {
+                    i=j+1;
+                    createdWord.clear();
+                }
+            }
+        }
+        if(createdWord.isEmpty()==true){
+            System.out.println("Word break True");
+        }
+        else
+            System.out.println("Word break False");
+    }
+
+
+    public static void main(String[] args){
+        String s = "catsandog";
+        String s1= "leetcode";
+        String s2= "applepenapple";
+        List<String> wordDict = new ArrayList<>(Arrays.asList("cats", "dog", "sand", "and", "cat"));
+        List<String> wordDict1 = new ArrayList<>(Arrays.asList("leet", "code"));
+        List<String> wordDict2 = new ArrayList<>(Arrays.asList("apple", "pen"));
+
+        WordBreakDynamic wordBreakDynamic=new WordBreakDynamic();
+        wordBreakDynamic.wordBreakSolution1(s1,wordDict1);
+    }
+}
