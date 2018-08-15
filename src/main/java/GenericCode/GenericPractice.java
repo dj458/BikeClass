ee8cc220cc60f7cd60134ce37770540244d5b776
dj458 <fzheng8@vt.edu>
2018-08-15 19:06:44 -0400

    multi parameter generic type

--- src/main/java/GenericCode/GenericPractice.java
+++ src/main/java/GenericCode/GenericPractice.java
@@ -2,18 +2,25 @@ package GenericCode;
 
 
 
-class Test<T>
+class Test<T,U>
 {
     T obj;
-    Test(T obj){this.obj=obj;}
+    U obj2;
+    Test(T obj, U obj2)
+    {
+        this.obj=obj;
+        this.obj2=obj2;
+    }
     public T getObj(){return this.obj;}
 }
 public class GenericPractice {
     public static void main(String[] args){
-        Test<Integer> iobj=new Test<Integer>(12312);
-        System.out.println(iobj.getObj());
-        Test<String> sobj=new Test<String>("wqweqwe");
-        System.out.println(sobj.getObj());
+        Test<Integer,String> iobj=new Test<Integer,String>(12312,"we2e21");
+        System.out.println(iobj.obj.toString()+iobj.obj2.toString());
+        Test<String,String> sobj=new Test<String,String>("wqweqwe","werwewqe");
+        System.out.println(sobj.obj.toString()+sobj.obj2.toString());
+        Test<Boolean,String> bobj=new Test<Boolean,String>(true,"qweqwe");
+        System.out.println(bobj.obj.toString()+bobj.obj2.toString());
     }
 
 }
