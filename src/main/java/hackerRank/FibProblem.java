8197f0f72da36a71663d4ba592659966f3203fb7
dj458 <fzheng8@vt.edu>
2018-11-19 17:36:17 -0500

    fib

---
+++ src/main/java/hackerRank/FibProblem.java
@@ -0,0 +1,14 @@
+package hackerRank;
+
+public class FibProblem {
+
+    public static int fib(int n){
+        if(n<2){return n;}
+        else return fib(n-1)+fib(n-2);
+
+    }
+
+    public static void main(String args[]){
+        System.out.println(FibProblem.fib(9));
+    }
+}
